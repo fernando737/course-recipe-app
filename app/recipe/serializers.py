9584c1f9853bd9385185aa355e2d674c725a8992
Fernando <fernando.perez@adidas.com>
2024-04-23 11:19:27 -0500

    fix upload images

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -101,4 +101,4 @@ class RecipeImageSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ['id', 'image']
         read_only_fields = ['id']
-        extra_kwargs = {'image': {'require': 'True'}}
+        extra_kwargs = {'image': {'required': 'True'}}
